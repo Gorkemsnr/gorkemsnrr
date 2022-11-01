a345c964921c5ada7eee5ddd26fe1e6bbcf58c8f
Gorkemsnr <114986809+Gorkemsnr@users.noreply.github.com>
2022-11-01 19:26:09 +0300

    Delete php login directory

--- php login/cikis.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "Başarıyla Çıkış Yaptınız. Tekrar Giriş Yapmak İçin <a href='login.php'>Tıklayın</a>";
-?>
--- php login/hosgeldin.php
+++
@@ -1,5 +0,0 @@
-<?php
- session_start();
- echo"Hoşgeldiniz ". $_SESSION['kullanici'];
- echo "<a href='cikis.php'>Çıkış</a>";
-?>
